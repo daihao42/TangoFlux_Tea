f12f810bdd6fef36e38e28164be1d5f640a0a04a
Hao Dai <h.dai@soton.ac.uk>
2025-08-18 16:31:28 +0100

    add num_per_prompt

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="tangoflux",
     description="TangoFlux: Super Fast and Faithful Text to Audio Generation with Flow Matching",
-    version="0.1.5",
+    version="0.1.6",
     packages=["tangoflux"],
     install_requires=[
         "torch==2.4.0",
